46b133a5077d1472f32181ad2d70fc4512abbc9c
Orbit001 <88408418+Orbit001@users.noreply.github.com>
2021-09-29 08:46:22 -0400

    main files

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("generate").onclick = generatePassword();
+
+function generatePassword() {
+  var length = Number(prompt("Desired password length?"));
+  var charType = prompt("Any preference? (special, numeric, uppercase, lowercase and random.");
+
+  var charSet = "";
+  var charTypeLower = charType.toLowerCase();
+  if( charTypeLower === "lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "uppercase" ) {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric" ) {
+    charSet = "0123456789";
+  } else if( charTypeLower === "special" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "random" ) {
+    charSet = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!0123456789\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  }
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
+}
+document.getElementById("password").value = (generatePassword());
